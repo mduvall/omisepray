e4f2297c9e40ecd9dc55aa61a2a80353b7911130
Matt DuVall <mduvall89@gmail.com>
2013-08-26 23:12:38 -0700

    Add initial states and function filtering for then

--- omisepray.js
+++ omisepray.js
@@ -1,10 +1,32 @@
 (function() {
   var root = this,
-      Promise = function() {};
+      Promise = function(resolveFunc) {
+
+      },
+      later = function(func, args) {
+        setImmediate(function() {
+          func(args);
+        });
+      };
 
   // Promises/A+ 2.1: Promise defines the 'then' function
-  Promise.prototype.then = function() {
-    return true;
+  Promise.prototype = {
+    constructor: Promise,
+    pending: undefined,
+    fulfilled: undefined,
+    rejected: undefined,
+
+    then: function(onFulfilled, onRejected) {
+      var promise = new this.constructor(function() {});
+
+      if (typeof onFulfilled !== "function") {
+        onFulfilled = undefined;
+      }
+
+      if (typeof onRejected !== "function") {
+        onRejected = undefined;
+      }
+    },
   };
 
   root.Promise = Promise;
--- test/omisepray.js
+++ test/omisepray.js
@@ -7,7 +7,26 @@ describe("Promise object", function() {
 describe("Promise interface", function() {
   it("should implement the 'then' function", function() {
     var promise = new Promise();
+
     expect(promise.then).toBeDefined();
     expect(promise.then()).toBeTruthy();
   });
+
+  it("should have all states initally be undefined", function() {
+    var promise = new Promise();
+
+    expect(promise.pending).not.toBeDefined();
+    expect(promise.fulfilled).not.toBeDefined();
+    expect(promise.rejected).not.toBeDefined();
+  });
+});
+
+describe("later", function() {
+  it("should call setImmediate and not return any value", function() {
+
+  });
+});
+
+describe("then", function() {
+
 });
