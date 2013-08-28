0efda1da57de985d547e7c3f7b36883582943ce7
Matt DuVall <mduvall89@gmail.com>
2013-08-27 23:42:37 -0700

    Constructor with resolution and state changes

--- omisepray.js
+++ omisepray.js
@@ -1,60 +1,93 @@
 (function() {
   var root = this,
-      Promise = function(resolveFunc, rejectFunc) {
+      Promise = function(func) {
+        // TODO: add validation for func
         var promise = this,
-            resolved = false,
             resolvedPromise = function(val) {
-              if (resolved) {
+              if (!promise.pending) {
                 return;
               }
-              resolved = true;
-              resolve(promise, val);
+
+              asyncStateChange(promise, "fulfilled", val);
             },
+
             rejectedPromise = function(val) {
-              if (resolved) {
+              if (!promise.pending) {
                 return;
               }
-              resolved = true;
-              reject(promise, val);
+
+              asyncStateChange(promise, "rejected", val);
             };
+
+        asyncStateChange(promise, "pending", null);
 
         try {
           // Try to resolve the function and pass in the current promise
-          resolve(promise, null);
+          func(resolvedPromise, rejectedPromise);
         } catch(e) {
-
+          rejectedPromise("error");
         }
       };
 
   function later(func, args) {
-    setImmediate(function() {
+    var f = function() {
       func(args);
+    };
+
+    if (typeof setImmediate === "function") {
+      setImmediate(f);
+    } else {
+      setTimeout(f, 1);
+    }
+  }
+
+  function asyncStateChange(promise, state, val) {
+    later(function() {
+      promise.changeState(state, val);
     });
   }
 
-  function resolve(promise, val) {
-    later(function() {
-      promise.stateChange('resolved');
-    }, null);
-  }
-
-  // Promises/A+ 2.1: Promise defines the 'then' function
+  // Promises/A+ 2.1: Promise defines the "then" function
   Promise.prototype = {
     constructor: Promise,
     pending: undefined,
     fulfilled: undefined,
     rejected: undefined,
+    value: undefined,
+    toFulfill: [],
+    toReject: [],
+
+    changeState: function(st, val) {
+      // No state changes from 'fulfilled' or 'rejected'
+      if (this.fulfilled || this.rejected) {
+        return;
+      }
+
+      this[st] = true;
+      this.value = val;
+
+      // If the promise has been fulfilled or rejected, it is no longer pending
+      if (st === "fulfilled" || st === "rejected") {
+        this.pending = false;
+      }
+    },
 
     then: function(onFulfilled, onRejected) {
       var promise = new this.constructor(function() {});
 
-      if (typeof onFulfilled !== "function") {
+      if (typeof onFulfilled === "function") {
+        toFulfill.push(onFulfilled);
+      } else {
         onFulfilled = undefined;
       }
 
-      if (typeof onRejected !== "function") {
+      if (typeof onRejected === "function") {
+        toReject.push(onRejected);
+      } else {
         onRejected = undefined;
       }
+
+      return true;
     },
   };
 
