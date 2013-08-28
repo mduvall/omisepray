7e79cd3f22b00e10672802273959e4ad95c8c68d
Matt DuVall <mduvall89@gmail.com>
2013-08-27 22:34:22 -0700

    Scaffold for resolving a promise

--- omisepray.js
+++ omisepray.js
@@ -1,13 +1,42 @@
 (function() {
   var root = this,
-      Promise = function(resolveFunc) {
+      Promise = function(resolveFunc, rejectFunc) {
+        var promise = this,
+            resolved = false,
+            resolvedPromise = function(val) {
+              if (resolved) {
+                return;
+              }
+              resolved = true;
+              resolve(promise, val);
+            },
+            rejectedPromise = function(val) {
+              if (resolved) {
+                return;
+              }
+              resolved = true;
+              reject(promise, val);
+            };
 
-      },
-      later = function(func, args) {
-        setImmediate(function() {
-          func(args);
-        });
+        try {
+          // Try to resolve the function and pass in the current promise
+          resolve(promise, null);
+        } catch(e) {
+
+        }
       };
+
+  function later(func, args) {
+    setImmediate(function() {
+      func(args);
+    });
+  }
+
+  function resolve(promise, val) {
+    later(function() {
+      promise.stateChange('resolved');
+    }, null);
+  }
 
   // Promises/A+ 2.1: Promise defines the 'then' function
   Promise.prototype = {
